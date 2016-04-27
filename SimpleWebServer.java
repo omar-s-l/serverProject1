73db11edc71d879fd3eac67b1b1cbe31c85f1a36
cjhodnefield <cjhodnefield@gmail.com>
2016-04-27 14:52:43 -0500

    Web server class updated

--- SimpleWebServer.java
+++ SimpleWebServer.java
@@ -1,3 +1,9 @@
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
+
+// Attributions: Several of these methods were taken from instructor solutions to Project0
 public class SimpleWebServer {
 	private final int serverNumber;
 	private ServerSocket socket;
@@ -7,9 +13,25 @@ public class SimpleWebServer {
 		this.serverPort = serverPort;
 	}
 
-	public void start() {
+	public void start() throws IOException {
 		socket = new ServerSocket(serverPort);
+		System.out.println("Server bound and listening to port " + serverPort);
 	}
+
+	public boolean acceptFromClient() throws IOException {
+		Socket clientSocket;
+		try {
+			clientSocket = socket.accept();
+		} catch (SecurityException e) {
+			System.out.println("Security manager intervened; your config is wrong. " + e);
+			return false;
+		} catch (IllegalArgumentException e) {
+			System.out.println("Probably an invalid port number. " + e);
+			return false;
+		}
+	}
+
+	
 
 	public static void main(String[] args) throws IOException {
 		portNumber = Integer.parseInt(args[0].substring(7));
