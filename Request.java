8612631c9d492da15a62144e89e2c39c4fa430d4
Omar Latif <omar-s-l@users.noreply.github.com>
2016-04-28 14:36:32 -0500

    First Request class

--- Request.java
+++ Request.java
@@ -1,3 +1,26 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+
 public class Request { 
 	
+	private String method;
+	private String path; 
+	//private ArrayList<ArrayList<String>> lines;
+
+
+	Request(ArrayList<ArrayList<String>> lines) {
+		this.method = lines.get(0).get(0)
+		this.path = lines.get(0).get(1)
+	}
+
+	public String getMethod(){
+		return this.method;
+	}
+
+	public String getPath(){
+		return this.path; 
+	}
+
 }
