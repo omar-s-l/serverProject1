8ee3581b82dae383a08676598d34018ae20c12c0
cjhodnefield <cjhodnefield@gmail.com>
2016-04-28 14:52:56 -0500

    Response updated

--- Response.java
+++ Response.java
@@ -8,14 +8,27 @@ public class Response {
 	private String date;
 	private SimpleDateFormat dateFormat;
 
-	public Response(int error, String contentType) {
+	public Response(int error, String fileType) {
 		this.error = error;
-		this.contentType = contentType;
+		this.contentType = interpretContentType(fileType);
 
 		// http://stackoverflow.com/questions/7707555/getting-date-in-http-format-in-java
 		dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
 		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
 		this.date = dateFormat.format(Calendar.getInstance().getTime());
+	}
+
+	public String interpretContentType(String fileType) {
+		String contentType = "";
+
+		switch (fileType) {
+			case ".txt":
+				contentType = "text/plain";
+			default:
+				contentType = "text/plain";
+		}
+
+		return contentType;
 	}
 
 	public String toString() {
