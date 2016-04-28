d1c12463ba0659480af12aefca4cb952ea789789
cjhodnefield <cjhodnefield@gmail.com>
2016-04-28 14:21:59 -0500

    Response class updated

---
+++ Response.java
@@ -0,0 +1,26 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class Response {
+	private String protocol = "HTTP/1.1";
+	private int error;
+	private String contentType;
+	private Date date;
+
+	public Response(int error, String contentType) {
+		this.error = error;
+		this.contentType = contentType;
+
+		this.date = Calendar.getInstance().getTime();
+	}
+
+	public String toString() {
+		String str = "";
+
+		str += protocol + " " + error + " OK\r\n";
+		str += "Content-Type: " + contentType + "\r\n";
+		str += "Date: " + date;
+
+		return str;
+	}
+}
